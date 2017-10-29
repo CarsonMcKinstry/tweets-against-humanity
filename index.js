7b25c4bd920bdfa46109929b85e7f18698090ad7
Carson McKinstry <carson.mckinstry@gmail.com>
2017-10-28 21:49:25 -0500

    You can now call the bot

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const twit = require("twit");
 const axios = require("axios");
+const _ = require("lodash");
 
 const Twitter = new twit({
   consumer_key: process.env.CONSUMER_KEY,
@@ -8,32 +9,96 @@ const Twitter = new twit({
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
-// axios.get(process.env.API_ENDPOINT)
-// .then(data => {
-//   console.log(data);
-// });
+const stream = Twitter.stream("statuses/filter", {
+  track: "#blackcard, #whitecard, @TweetsAHumanity",
+  language: "en"
+});
 
-const getAnswer = () => {
-  axios.get(process.env.API_ENDPOINT).then(data => {
+const getAnswer = (responseId, originalAuthor) => {
+  console.log("got to getAnswer");
+  axios.get(process.env.API_ENDPOINT + "/build").then(data => {
+    console.log(responseId);
+
     let answer = data.data.answer + " #CAH #TAH";
-    answer.length <= 140 ? createPost(answer) : getAnswer();
+
+    if (originalAuthor) {
+      answer = "@" + originalAuthor + " " + answer;
+    }
+    answer.length <= 140
+      ? createPost(answer, responseId)
+      : getAnswer(responseId, originalAuthor);
   });
 };
 
-const createPost = answer => {
-  Twitter.post(
-    "statuses/update",
-    {
-      status: answer
-    },
-    (err, res) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      console.log(res);
+const createPost = (answer, responseId) => {
+  const post = {
+    status: answer
+  };
+  if (responseId) {
+    post.in_reply_to_status_id = responseId;
+  }
+  console.log("Got to post", post);
+  Twitter.post("statuses/update", post, (err, res) => {
+    if (err) {
+      console.error(err);
+      return;
     }
-  );
+    console.log("Tweet succesfully replied to");
+  });
 };
 
 setInterval(getAnswer, process.env.INTERVAL * 60 * 60 * 1000);
+
+const answerTweet = (responseId, originalAuthor) => {
+  axios.get(process.env.API_ENDPOINT + "/white?pick=1").then(data => {
+    let answer = data.data[0].text + " #CAH #TAH";
+
+    if (originalAuthor) {
+      answer = "@" + originalAuthor + " " + answer;
+    }
+    answer.length <= 140
+      ? createPost(answer, responseId)
+      : answerTweet(responseId, originalAuthor);
+  });
+};
+
+const emptyReply = (answer, responseId, originalAuthor) => {
+  const tweet = "@" + originalAuthor + " " + answer + " #CAH #TAH";
+  createPost(tweet, responseId);
+};
+
+const answerText = tweet => {
+  const originalAuthor = tweet.user.screen_name;
+  const tweetText = tweet.text.split(" ");
+  const responseId = tweet.id_str;
+
+  if (tweetText.indexOf("@TweetsAHumanity") !== -1) {
+    const mappedTweetText = tweetText.map(text => text.toLowerCase());
+
+    const blackCardCheck = mappedTweetText.indexOf("#blackcard") !== -1;
+    const whiteCardCheck = mappedTweetText.indexOf("#whitecard") !== -1;
+
+    const filteredText = mappedTweetText.filter(text => {
+      return !_.startsWith(text, "#");
+    });
+
+    const isQuestion = _.chain(filteredText)
+      .last()
+      .endsWith("?")
+      .value();
+
+    if (blackCardCheck && whiteCardCheck) {
+      getAnswer(responseId, originalAuthor);
+    } else if (whiteCardCheck && isQuestion) {
+      answerTweet(responseId, originalAuthor);
+    } else {
+      emptyReply("What do you want from me!?", responseId, originalAuthor);
+    }
+  } else {
+    console.error("That tweet didn't contain my username");
+  }
+};
+
+stream.on("tweet", answerText);
+
+// answerText({ text: "@TweetsAHumanity What's the meaning of life? #whitecard" });
